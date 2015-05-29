e7119e65253e71b60be2744f56732cfb3fea8c1e
Gerardo <gperegrina94@gmail.com>
2015-05-29 11:05:05 -0700

    Update gPeregrina.cpp

--- gPeregrina.cpp
+++ gPeregrina.cpp
@@ -12,32 +12,8 @@ typedef double Arr[3];
     Arr pos;
     Arr vel;
 };*/
-/*
-//First Duck Sprite
-Sprite duck_sprite;
-Ppmimage *duckImage=NULL;
-GLuint duckTexture;
-GLuint duckSil;
-//Second Duck Sprite
-Sprite duck_sprite2;
-Ppmimage *duckImage2=NULL;
-GLuint duckTexture2;
-GLuint duckSil2;
-int show_duck = 0;
-int silhouette = 1;
-//Bullet Sprite
-Sprite bullet_sprite;
-Ppmimage *bulletImage=NULL;
-GLuint bulletTexture;
-//White duck Sprite
-Sprite duckscore_sprite;
-Ppmimage *duckscoreImage=NULL;
-GLuint duckscoreTexture;
-//Red duck sprite
-Sprite duckscore_sprite2;
-Ppmimage *duckscoreImage2=NULL;
-GLuint duckscoreTexture2;
-*/
+
+
 void set_title(void)
 {
     //Set the window title bar.
@@ -205,6 +181,49 @@ void init_opengl(void)
     //-------------------------------------------------------------------
 
     //-------------------------------------------------------------------
+	//duck sprite 3
+	glGenTextures(1, &duckTexture3);
+	glGenTextures(1, &duckSil3);
+	duckImage3 = ppm6GetImage("./images/shotduck1.ppm");
+	int w6 = duckImage3->width;
+	int h6 = duckImage3->height;
+	glBindTexture(GL_TEXTURE_2D, duckTexture3);
+	//
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w6, h6, 0, GL_RGB, GL_UNSIGNED_BYTE, duckImage3->data);
+	//-------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------
+	//duck sprite 4
+	glGenTextures(1, &duckTexture4);
+	glGenTextures(1, &duckSil4);
+	duckImage4 = ppm6GetImage("./images/shotduck2.ppm");
+	int w7 = duckImage4->width;
+	int h7 = duckImage4->height;
+	glBindTexture(GL_TEXTURE_2D, duckTexture4);
+	//
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w7, h7, 0, GL_RGB, GL_UNSIGNED_BYTE, duckImage4->data);
+	//-------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------
+	//duck sprite 5
+	glGenTextures(1, &duckTexture5);
+	glGenTextures(1, &duckSil5);
+	duckImage5 = ppm6GetImage("./images/ducks2.ppm");
+	int w8 = duckImage5->width;
+	int h8 = duckImage5->height;
+	glBindTexture(GL_TEXTURE_2D, duckTexture5);
+	//
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w8, h8, 0, GL_RGB, GL_UNSIGNED_BYTE, duckImage5->data);
+	//-------------------------------------------------------------------
+
+
+    //-------------------------------------------------------------------
     //duck silhouette 
     glBindTexture(GL_TEXTURE_2D, duckSil);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
@@ -227,6 +246,120 @@ void init_opengl(void)
             GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData2);
     delete [] silhouetteData2;
     //-------------------------------------------------------------------
+
+    //-------------------------------------------------------------------
+	//duck silhouette 3 
+	glBindTexture(GL_TEXTURE_2D, duckSil3);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	////must build a new set of data...
+	unsigned char *silhouetteData3 = buildAlphaData(duckImage3);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w6, h6, 0,
+	GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData3);
+	delete [] silhouetteData3;
+	//-------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------
+	//duck silhouette 4 
+	glBindTexture(GL_TEXTURE_2D, duckSil4);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	////must build a new set of data...
+	unsigned char *silhouetteData4 = buildAlphaData(duckImage4);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w7, h7, 0,
+	GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData4);
+	delete [] silhouetteData4;
+	//-------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------
+	//duck silhouette 5
+	glBindTexture(GL_TEXTURE_2D, duckSil5);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	////must build a new set of data...
+	unsigned char *silhouetteData5 = buildAlphaData(duckImage5);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w8, h8, 0,
+	GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData5);
+	delete [] silhouetteData5;
+	//-------------------------------------------------------------------
+
+    //-------------------------------------------------------------------
+	//Dog sprite 1
+	glGenTextures(1, &dogTexture1);
+	glGenTextures(1, &dogSil1);
+	dogImage1 = ppm6GetImage("./images/dogs1.ppm");
+	int w10 = dogImage1->width;
+	int h10 = dogImage1->height;
+	glBindTexture(GL_TEXTURE_2D, dogTexture1);
+	//
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w10, h10, 0, GL_RGB, GL_UNSIGNED_BYTE, dogImage1->data);
+	//-------------------------------------------------------------------
+
+	//-------------------------------------------------------------------
+	//Dog sprite 2
+	glGenTextures(1, &dogTexture2);
+	glGenTextures(1, &dogSil2);
+	dogImage2 = ppm6GetImage("./images/dogs2.ppm");
+	int w11 = dogImage2->width;
+	int h11 = dogImage2->height;
+	glBindTexture(GL_TEXTURE_2D, dogTexture2);
+	//
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w11, h11, 0, GL_RGB, GL_UNSIGNED_BYTE, dogImage2->data);
+	//-------------------------------------------------------------------
+
+	//-------------------------------------------------------------------
+	//Dog sprite 3
+	glGenTextures(1, &dogTexture3);
+	glGenTextures(1, &dogSil3);
+	dogImage3 = ppm6GetImage("./images/dog3.ppm");
+	int w12 = dogImage3->width;
+	int h12 = dogImage3->height;
+	glBindTexture(GL_TEXTURE_2D, dogTexture3);
+	//
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, w12, h12, 0, GL_RGB, GL_UNSIGNED_BYTE, dogImage3->data);
+	//-------------------------------------------------------------------
+
+    //-------------------------------------------------------------------
+	//Dog silhouette 1
+	glBindTexture(GL_TEXTURE_2D, dogSil1);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	////must build a new set of data...
+	unsigned char *silhouetteData6 = buildAlphaData(dogImage1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w10, h10, 0,
+	GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData6);
+	delete [] silhouetteData6;
+	//-------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------
+	//Dog silhouette 2
+	glBindTexture(GL_TEXTURE_2D, dogSil2);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	////must build a new set of data...
+	unsigned char *silhouetteData7 = buildAlphaData(dogImage2);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w11, h11, 0,
+	GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData7);
+	delete [] silhouetteData7;
+	//-------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------
+	//Dog silhouette 3
+	glBindTexture(GL_TEXTURE_2D, dogSil3);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	////must build a new set of data...
+	unsigned char *silhouetteData8 = buildAlphaData(dogImage3);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w12, h12, 0,
+	GL_RGBA, GL_UNSIGNED_BYTE, silhouetteData8);
+	delete [] silhouetteData8;
+	//-------------------------------------------------------------------
 
 //-------------------------------------------------------------------
     //background textures
